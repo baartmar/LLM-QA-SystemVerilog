9e5b5ae40e8df5d2a955359b954abc203685cb5c
Raymond <n/a>
2024-05-28 14:21:53 -0700

    updated run script

--- runme.sh
+++ runme.sh
@@ -1,4 +1,5 @@
 source "/nfs/stak/users/baartmar/hpc-share/LLM-QA-SystemVerilog/.venv/bin/activate"
+module load cuda/12.1
 export HUGGINGFACE_HUB_CACHE="/nfs/stak/users/baartmar/hpc-share/LLM-QA-SystemVerilog/.cache"
 export HF_DATASETS_CACHE="/nfs/stak/users/baartmar/hpc-share/LLM-QA-SystemVerilog/.datasets"
 python3 run.py
