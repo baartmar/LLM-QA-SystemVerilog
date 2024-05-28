70699a9fe1714a35569a62be73a770b0964949f8
Raymond <n/a>
2024-05-28 14:19:21 -0700

    run script

---
+++ runme.sh
@@ -0,0 +1,4 @@
+source "/nfs/stak/users/baartmar/hpc-share/LLM-QA-SystemVerilog/.venv/bin/activate"
+export HUGGINGFACE_HUB_CACHE="/nfs/stak/users/baartmar/hpc-share/LLM-QA-SystemVerilog/.cache"
+export HF_DATASETS_CACHE="/nfs/stak/users/baartmar/hpc-share/LLM-QA-SystemVerilog/.datasets"
+python3 run.py
