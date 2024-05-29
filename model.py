7c686fc564f2cd7fad23b767ab9cb4b0b5e49c2b
Raymond Baartmans <baartmar@dgx2-1.hpc.engr.oregonstate.edu>
2024-05-29 09:37:47 -0700

    some changes

--- model.py
+++ model.py
@@ -4,7 +4,7 @@ from transformers import T5Tokenizer, T5ForConditionalGeneration, AutoTokenizer,
 #import openai
 import torch
 from evaluate import load
-import tqdm
+from tqdm import tqdm
 import json
 import os
 
@@ -18,7 +18,7 @@ class LLM():
         self.config = AutoConfig.from_pretrained(model_name)
         if self.config.model_type == 't5':
             self.tokenizer = T5Tokenizer.from_pretrained(tokenizer_name if tokenizer_name is not None else model_name)
-            
+
             self.llm = T5ForConditionalGeneration.from_pretrained(model_name, device_map='auto', torch_dtype=torch.float16)
         elif any(chat_keyword in self.model_name for chat_keyword in ["it", "chat", "Instruct", "vicuna"]):
             self.tokenizer = AutoTokenizer.from_pretrained(tokenizer_name if tokenizer_name is not None else model_name)
@@ -35,16 +35,16 @@ class LLM():
         if 'no answer' in output.lower():
             prediction['no_answer_probability'] = 1.
         return prediction, reference
-        
+
     def evaluate_model(self, data:SystemVerilogDataset, output_dir):
         predictions, references = [], []
         for idx, question in enumerate(tqdm(data.questions)):
             prediction, reference = self.answer_question(question)
             predictions.append(prediction)
             references.append(reference)
-        
+
         results = squad_metric.compute(predictions=predictions, references=references)
-        with open(os.path.join(output_dir, f'{self.model_name}.json'), 'w') as f:
+        with open(os.path.join(output_dir, f'{self.model_name.replace("/", "-")}.json'), 'w') as f:
             json.dump({'predictions': predictions, 'references': references}, f)
         results['model'] = self.model_name
         return results
